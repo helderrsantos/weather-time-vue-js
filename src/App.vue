ef4f7efe63598274fd6d0451cf94c18c2edeb098
helder <helderr_santos@hotmail.com>
2023-08-08 02:37:27 -0300

    feat: initial project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Card from "./components/Card.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Card msg="Agora foi!" />
+    <Card msg="Agora foi!" />
+    <Card msg="Agora foi!" />
+    <Card msg="Agora foi!" />
+  </div>
+</template>
+
+<style>
+.container {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.2);
+  margin-top: 8%;
+  margin-right: 45%;
+}
+
+div {
+}
+</style>
---
+++ src/components/CardMain.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1 class="title">{{ msg }}</h1>
+</template>
+
+<style>
+.title {
+  text-align: center;
+}
+</style>
